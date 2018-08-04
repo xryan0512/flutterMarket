f75a262ba04780614daf0cecc76b2c921c83ca02
Bryan Alvarez <bryandev12@gmail.com>
2018-08-03 17:41:42 -0800

    flutter market

---
+++ lib/category_route.dart
@@ -0,0 +1,78 @@
+
+
+import 'package:flutter/material.dart';
+
+import 'package:task_02_category_widget/category.dart';
+
+final _backgroundColor = Colors.deepOrangeAccent;
+
+class CategoryRoute extends StatelessWidget {
+  const CategoryRoute();
+
+  static const _categoryNames = <String>[
+    'Pizza',
+    'Hamburgue',
+    'Spaguetti',
+    'Sushi',
+    'Empanadas',
+    'Bebidas Alcoholicas',
+    'Redbull',
+    'Capitan Cruch',
+  ];
+
+  static const _baseColors = <Color>[
+    Colors.teal,
+    Colors.orange,
+    Colors.pinkAccent,
+    Colors.blueAccent,
+    Colors.yellow,
+    Colors.greenAccent,
+    Colors.purpleAccent,
+    Colors.red,
+  ];
+
+
+  Widget _buildCategoryWidgets(List<Widget> categories) {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) => categories[index],
+      itemCount: categories.length,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final categories = <Category>[];
+
+    for (var i = 0; i < _categoryNames.length; i++) {
+      categories.add(Category(
+        name: _categoryNames[i],
+        color: _baseColors[i],
+        iconLocation: Icons.favorite_border,
+      ));
+    }
+
+    final listView = Container(
+      color: _backgroundColor,
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      child: _buildCategoryWidgets(categories),
+    );
+
+    final appBar = AppBar(
+      elevation: 0.0,
+      title: Text(
+        'Elige tu preferida',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 30.0,
+        ),
+      ),
+      centerTitle: true,
+      backgroundColor: _backgroundColor,
+    );
+
+    return Scaffold(
+      appBar: appBar,
+      body: listView,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+import 'package:task_02_category_widget/category_route.dart';
+
+void main() {
+  runApp(UnitConverterApp());
+}
+
+
+class UnitConverterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Unit Converter',
+      home: CategoryRoute(),
+    );
+  }
+}
